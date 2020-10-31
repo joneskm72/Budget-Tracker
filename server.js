72e8df762e6cd82af3eafda83a52d59ceb820a09
Kasey Jones <joneskm72@gmail.com>
2020-10-31 09:00:09 -0400

    Testing h10 error

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-app.listen(port, "0.0.0.0", function() {
-  console.log('Listening on Heroku defined port');
-});
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
