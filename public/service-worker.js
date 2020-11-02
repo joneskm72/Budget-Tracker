fc33adb56766b824caa7a02f67705d63bf13f388
Kasey Jones <joneskm72@gmail.com>
2020-11-02 18:50:45 -0500

    Cleaned up code

--- public/service-worker.js
+++ public/service-worker.js
@@ -44,23 +44,22 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   if (event.request.url.includes("/api/")) {
     event.respondWith(
-    caches.open(DATA_CACHE).then(cache => {
-      return fetch(event.request).then(res => {
-        if (res.status === 200) {
-          cache.put(event.request.url, response.clone())
-        }
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(event.request).then(res => {
+          if (res.status === 200) {
+            cache.put(event.request.url, response.clone())
+          }
         return response
-      }).catch(err => {
+        }).catch(err => {
         return cache.match(event.request)
-      });
-    }).catch(err => console.log(err))
+        });
+      }).catch(err => console.log(err))
     );
-
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     }) 
    );
