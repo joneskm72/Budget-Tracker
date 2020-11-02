2d30f06c66798d28cfc3df7903b4e4fbb7789664
Kasey Jones <joneskm72@gmail.com>
2020-11-02 16:40:23 -0500

    Added additional files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,9 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-512x512.png",
+  "/https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "/https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 const STATIC_CACHE = "static-cache-v2";
